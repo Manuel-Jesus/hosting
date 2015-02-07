15c77708ed3e4cf06028b5c3b641ebcb044f89d0
Manuel Jesus Begines Gonzalez <manuelj@Mark-X>
2015-02-07 21:31:21 +0100

    Primer commit con primeras líneas

---
+++ README.md
@@ -0,0 +1,6 @@
+# hosting
+
+
+En este proyecto crearemos un script que nos permita el hospedaje de paginas
+web en un servidor que tengamos .
+
---
+++ script_alta.py
@@ -0,0 +1,13 @@
+import sys
+
+
+
+#pedimos el nombre y el dominio que se usara
+
+nombre = (sys.argv[1])
+dominio = (sys.argv[2])
+
+
+
+
+#siguiente paso: conectarnos a la bbdd
