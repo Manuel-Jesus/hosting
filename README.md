f9343aec9c57b1e95d33dbb8090a68f966437a4c
Manuel Jesus Begines Gonzalez <manueljesusbeginesgonzalez@gmail.com>
2015-02-08 20:30:45 +0100

    modificado el README

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 
 En este proyecto crearemos un script que nos permita el hospedaje de paginas
-web en un servidor que tengamos .
+web en un servidor que tengamos . Es un proyecto con fines educativos.
 
--- script_alta.py
+++ script_alta.py
@@ -1,11 +1,11 @@
 import sys
-
+import MySQLdb
 
 
 #pedimos el nombre y el dominio que se usara
 
-nombre = (sys.argv[1])
-dominio = (sys.argv[2])
+#nombre = (sys.argv[1])
+#dominio = (sys.argv[2])
 
 nombre = raw_input("Introduce tu nombre de usuario: ")
 dominio = raw_input("Introduce el nombre de dominio: ")
@@ -15,3 +15,30 @@ dominio = raw_input("Introduce el nombre de dominio: ")
 
 
 #siguiente paso: conectarnos a la bbdd
+
+conexion = MySQLdb.connect(host = "localhost", user = "root", passwd = "usuario", db = "ftpd")
+cursor = conexion.cursor()
+
+#comprobacion de si el usuario existe
+
+consulta_usuario = "select username from usuarios where username = '%s';" %nombre
+cursor.execute(consulta_usuario)
+consulta_usuario = cursor.fetchone()
+
+if consulta_usuario != None:
+	print "El nombre de usuario ya esta registrado"
+	sys.exit
+else:
+	consulta_dominio = "select ndominio from usuarios where ndominio = '%s';" %dominio
+	cursor.execute(consulta_dominio)
+	consulta_dominio = cursor.fetchone()
+
+	if consulta_dominio != None:
+		print "El nombre de dominio ya esta registrado"
+		sys.exit
+	else:
+		print "Todo correcto"
+
+
+
+
