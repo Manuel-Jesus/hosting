8ae0c3fd5789faa61c4668697832e94f1043df1a
Manuel Jesus Begines Gonzalez <manueljesusbeginesgonzalez@gmail.com>
2015-02-09 18:24:57 +0100

    Quitado los raw input ya que queremos que pasarle el nombre y el dominio por parametros, no que nos lo pida

--- script_alta.py
+++ script_alta.py
@@ -4,11 +4,8 @@ import os
 
 #pedimos el nombre y el dominio que se usara
 
-#nombre = (sys.argv[1])
-#dominio = (sys.argv[2])
-
-nombre = raw_input("Introduce tu nombre de usuario: ")
-dominio = raw_input("Introduce el nombre de dominio: ")
+nombre = (sys.argv[1])
+dominio = (sys.argv[2])
 
 
 #siguiente paso: conectarnos a la bbdd
