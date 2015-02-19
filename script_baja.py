46245561ad9e1c2b0229a5520ec286d036e4d99c
Manuel Jesus Begines Gonzalez <manueljesusbeginesgonzalez@gmail.com>
2015-02-19 12:45:05 +0100

    completamos el script de baja

--- script_baja.py
+++ script_baja.py
@@ -24,4 +24,22 @@ else:
         os.system("rm -r /etc/apache2/sites-available/%s"%dominio)
         #reiniciamos apache
         os.system("service apache2 restart>/dev/null")
-        
+        #borramos el usuario de mysql y las bases de datos
+        borrarbases="drop database my%s"%nombre
+        cursor.execute(borrarbases)
+        base.commit()
+        borrausuario=" drop user my%s@localhost" %nombre
+        cursor.execute(borrausuario)
+        base.commit()
+        #borramos la columna de la tabla ftpd
+        borracolumna="delete from usuarios where ndominio='%s';" %dominio
+        cursor.execute(borracolumna)
+        base.commit()
+        base.close()
+        #borramos el fichero db. donde 
+        os.system("rm -r /var/cache/bind/db.%s" %dominio)
+        #borramos la zona del fichero named.conf.local
+        os.system("sed '/zone " + '"%s"'% dominio + "/,/};/d' /etc/bind/named.conf.local > temporal")
+        os.system("mv temporal /etc/bind/named.conf.local")
+        print "El usuario fue elimindado correctamente y ya no está en el hosting"
+
